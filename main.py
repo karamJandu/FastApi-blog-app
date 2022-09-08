3ea8855d782d77e8e68a5e3c6e69d469da218165
Karamjeet <janduk@student.douglascollege.ca>
2022-09-08 16:23:35 -0700

    route with path and query params created

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
+from typing import Optional
+
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {'data': {'name': 'John'}}
+@app.get("/blog")
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs from the database'}
+    else:
+        return {'data': f'{limit} blogs from the database'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int, limit=10):
+    return {'data': ['1', '2']}
 
 
 @app.get('/about')
