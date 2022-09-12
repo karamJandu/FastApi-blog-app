37d8a3949df357eb0ed4cc72478821bf6568ddfa
Karamjeet <janduk@student.douglascollege.ca>
2022-09-11 21:48:30 -0700

    controller created

--- blog/router/blog.py
+++ blog/router/blog.py
@@ -4,42 +4,29 @@ from sqlalchemy.orm import Session
 
 from .. import schemas, models
 from ..database import get_db
+from ..controller import blog
 
 router = APIRouter(prefix="/blog", tags=["Blog"])
 
 @router.get('/', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    return blog.get_all(db)
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.BlogBase, db : Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+   return blog.create(request, db)
 
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def show(id, response: Response,  db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-         detail=f'Blog with the id {id} is not available.')
-    return blog
+def show(id, db: Session = Depends(get_db)):
+    return blog.show(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.BlogBase, db: Session =  Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update({'title': request.title, 'body': request.body})
-    db.commit()
-    return {'detail': 'Updated Successfully'}
-
+    return blog.update(id, request, db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
-    db.commit()
-    return {'detail': f'Blog {id} deleted successfully'}
+    return blog.destroy(id, db)
